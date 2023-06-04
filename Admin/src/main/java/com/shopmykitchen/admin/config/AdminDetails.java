d945fb13414a70732950d31915a3c17c15f70109
Asma Farooq <asma.faarook@gmail.com>
2023-06-03 20:47:15 -0700

    Admin Module Config

---
+++ Admin/src/main/java/com/shopmykitchen/admin/config/AdminDetails.java
@@ -0,0 +1,59 @@
+package com.shopmykitchen.admin.config;
+
+
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import com.shopmykitchen.library.model.Admin;
+import com.shopmykitchen.library.model.Role;
+
+
+
+public class AdminDetails implements UserDetails {
+
+    private Admin admin;
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        for(Role role:admin.getRoles()){
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        }
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return admin.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return admin.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ Admin/src/main/java/com/shopmykitchen/admin/shopmykitchen.java
@@ -0,0 +1,13 @@
+package com.shopmykitchen.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class shopmykitchen {
+
+	public static void main(String[] args) {
+		SpringApplication.run(shopmykitchen.class, args);
+	}
+
+}
